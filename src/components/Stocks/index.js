a7834c17789719bbca820c8ad4428b4c1c7a39cb
Chris <Chris.W.Carucci@Gmail.com>
2022-12-21 01:24:16 -0330

    initial commit

--- src/components/Stocks/index.js
+++ src/components/Stocks/index.js
@@ -1,7 +1,23 @@
+import { Redirect, useHistory } from "react-router-dom";
+
 function Stocks() {
+  let loggedIn = true;
+
+  if (loggedIn === false) {
+    return <Redirect to="/not-logged-in" />;
+  }
+
+  const history = useHistory();
+
+  const handleClick = () => {
+    window.alert("Sending info to the DB!");
+    history.push("/");
+  };
+
   return (
-    <div className='comp orange'>
+    <div className="comp orange">
       <h1>Stocks Component</h1>
+      <button onClick={handleClick}>Home</button>
     </div>
   );
 }
